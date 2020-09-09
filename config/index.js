80309b0ab179209b5048e06467b2be87c37b7241
Flavio Gava Milani <milanidesign7@gmail.com>
2020-09-09 11:41:10 -0300

    fix absolute path at build deploy

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/projeto-github-vue',
 
     /**
      * Source Maps
